56ebef774649adbe55b0f3a5a9e0e0d302c2400d
Ivan_Polovnikov <i.a.polovnikov@gmail.com>
2021-05-20 21:38:28 +0700

    homework 7 spring data

---
+++ src/main/java/com/ttsr/springdatademo/model/Product.java
@@ -0,0 +1,25 @@
+package com.ttsr.springdatademo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    Long id;
+
+    @Column
+    String name;
+
+    @Column
+    BigDecimal price;
+}
---
+++ src/main/java/com/ttsr/springdatademo/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.ttsr.springdatademo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ttsr.springdatademo.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByName(String name);
+
+    List<Product> findAllByPriceGreaterThan(BigDecimal min);
+
+    List<Product> findAllByPriceLessThan(BigDecimal max);
+
+    List<Product> findAllByPriceBetween(BigDecimal min, BigDecimal max);
+}
